df5cfdeca806dcabc2a22145cc0cf336e36f654f
Chirac-zibi <zibiarnaud859@gmail.com>
2025-12-29 01:21:58 +0100

    mon git P

---
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Projets_FILE C:/Projets/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/Projets.exe)
+set(__QT_DEPLOY_TARGET_Projets_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Projets_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
